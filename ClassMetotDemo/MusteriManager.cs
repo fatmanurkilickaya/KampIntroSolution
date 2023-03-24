6fe0432b3fde49600417f3bec89c28e4ef0268db
Fatma Nur Kılıçkaya <fatmanurkilickaya@hotmail.com>
2023-03-24 14:39:41 +0300

    Musteri ve MusteriManager oluşturuldu

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+     class MusteriManager
+     {
+        public void MusteriEkle(Musteri musteri) { 
+
+            Console.WriteLine(musteri.Name+ " " + musteri.Surname+ "isimli müşteri sisteme eklenmiştir.");
+        }
+
+        public void MusteriSil(Musteri musteri) {
+
+            Console.WriteLine(musteri.Id + "id numaralı müşteri sistemden silinmiştir.");
+        }
+
+        public void MusteriGuncelle(Musteri musteri) {
+            Console.WriteLine(musteri.Id + "id numaralı müşterinin bilgileri güncellenmiştir.");
+        }    
+
+     }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ClassMetotDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Name = "Nur";
+            musteri1.Surname = "Matilda";
+            musteri1.Age = 30;
+            musteri1.City = "Kayseri";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Name = "Maya";
+            musteri2.Surname = "Nur";
+            musteri2.Age = 30;
+            musteri2.City = "Aksaray";
+
+
+
+            MusteriManager musteriler = new MusteriManager();
+            musteriler.MusteriEkle(musteri1);
+            musteriler.MusteriSil(musteri2);
+            musteriler.MusteriGuncelle(musteri1);
+        }
+    }
+}
