26778d530bd8da54d3672738ce5836928ec62e0b
Fatma Nur Kılıçkaya <fatmanurkilickaya@hotmail.com>
2023-03-24 15:34:06 +0300

    Yazılım geliştirici kampı ders 4

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        public void Add()
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitsInStock = 10;
+
+            Product product2 = new Product
+            {
+                Id = 2,
+                CategoryId = 3,
+                ProductName = "Bilgisayar",
+                UnitsInStock = 10
+            };
+
+            Product product3 = new Product{Id = 5,CategoryId = 2, ProductName = "Kalem", UnitsInStock = 10};
+        }
+    }
+}
